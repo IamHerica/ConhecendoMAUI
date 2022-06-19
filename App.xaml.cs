342637bb32eaee579d5faa97975752d46dc5acbd
iamherica <hericacadoni@gmail.com>
2022-06-18 22:59:29 -0300

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ConhecendoMAUI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿namespace ConhecendoMAUI;
+
+public partial class MainPage : ContentPage
+{
+    int count = 0;
+
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private async void RedirectToLink(object sender, EventArgs e)
+    {
+        count++;
+
+        if (count == 1)
+        {
+            Uri uri = new Uri("https://www.instagram.com/iamhericaa/");
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+        else
+        {
+            Uri uri = new Uri("https://letmegooglethat.com/?q=por+que+as+pessoas+clicam+tantas+vezes+em+um+bot%C3%A3o%3F");
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
+        }
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
